41b9e883b981ed2ddbb80c5b3952c62cbd7910eb
energoww <enerGOww@gmail.com>
2020-09-30 12:07:42 +0300

    connect typeorm to project

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 
+config();
 @Module({
   providers: [ConfigService],
   exports: [ConfigService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      migrations: ["dist/migration/*.js"],
+      cli: {
+        "migrationsDir": "src/migration",
+        "entitiesDir": "src"
+      }
+    }),
+  ],
 })
 export class ConfigModule {}
