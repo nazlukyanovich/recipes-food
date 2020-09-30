a8bd0a4a31025300e485a795e252ae096cfcfd74
energoww <enerGOww@gmail.com>
2020-09-30 13:38:22 +0300

    fix test not close app

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,6 +21,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    await app.close()
+  })
+
   it('/hello (GET)', () => {
     return request(app.getHttpServer())
       .get('/hello')
