0926050ea008f42a102e698fb8e053ce90823a81
AleksandrGogolushko <gogolushko@outlook.com>
2020-10-07 12:37:27 +0300

    add image-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,38 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                optimizationLevel: 4
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
+      }
     ],
   },
   resolve: {
