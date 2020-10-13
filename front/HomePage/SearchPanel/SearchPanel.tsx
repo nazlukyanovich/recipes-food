e5fa0d6bb490f3ce7841eb53fc5badba430c3c39
AleksandrGogolushko <gogolushko@outlook.com>
2020-10-13 20:21:24 +0300

    add autocomplate

--- front/HomePage/SearchPanel/SearchPanel.tsx
+++ front/HomePage/SearchPanel/SearchPanel.tsx
@@ -1,25 +1,48 @@
 import * as React from 'react';
+import Chip from '@material-ui/core/Chip';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import './SearchPanel.css';
 import { CgSearch } from 'react-icons/cg';
-import { IngredientsWrapper } from './IngredientsWrapper/IngredientsWrapper';
 export const SearchPanel: React.FunctionComponent = () => {
-  let ingredients = ['apple', 'meat', 'tomato', 'carrot', 'pepper', 'onion'];
-  const [valueInput, setValueInput] = React.useState('');
-
-  const inputHandler = value => {
-    setValueInput(value);
-  };
-
+  const [valueInput, setValueInput] = React.useState(null);
+  const ingredientsData = [
+    { type: 'apple' },
+    { type: 'tomato' },
+    { type: 'pepper' },
+    { type: 'kiwi' },
+    { type: 'lemon' },
+    { type: 'orange' },
+    { type: 'carrot' },
+    { type: 'onion' },
+    { type: 'pumpkin' },
+    { type: 'turnips' },
+  ];
   return (
     <div className="SearchPanel">
       <div className="SearchPanel_wrapper">
         <form>
           <div className="SearchPanel_searchInput">
-            <input
-              value={valueInput}
-              onChange={e => inputHandler(e.target.value)}
-              type="text"
-              name="searchIngredients"
+            <Autocomplete
+              multiple
+              id="auto-complete"
+              options={ingredientsData}
+              getOptionLabel={option => option.type}
+              onChange={(event, value) => {
+                setValueInput(value);
+              }}
+              renderInput={params => (
+                <div ref={params.InputProps.ref}>
+                  <input
+                    value={valueInput}
+                    // onChange={e => setValueInput(e.target.value)}
+                    type="text"
+                    name="searchIngredients"
+                    {...params.inputProps}
+                  />
+                </div>
+              )}
             />
           </div>
           <div className="SearchPanel_buttonGroup">
